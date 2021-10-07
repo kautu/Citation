4857e6b015feacdb2bc988b8c803c779a2fe6b1d
kautu <l3esie@gmail.com>
2021-10-07 13:43:29 +0800

    Update and rename Clusterng_Interpretate.R to Clusterng_Isolation.R

--- DATASET/Clusterng_Isolation.R
+++ DATASET/Clusterng_Isolation.R
@@ -49,3 +49,48 @@ energy.sparse.top <- energy.sparse %>%
 
 write.csv(energy.sparse.top, 'cluster_character.csv', row.names = FALSE)
 
+
+
+
+##
+energy.isolate <- subset(energy.triadic, !FC %in% unique(c(edges.sparse$node))) %>% 
+  group_by(FC, IPC) %>% 
+  summarise(count = n()) %>% 
+  left_join(node.centrality, by = c('FC' = 'node')) %>% 
+  group_by(membership, IPC) %>% 
+  summarise(num = n(),
+            count = sum(count)) %>% 
+  filter(is.na(membership) == FALSE ) %>% 
+  mutate(membership = as.factor(membership)) %>% 
+  arrange(membership, -count) %>% 
+  slice_max(count, n = 3) %>% 
+  #left_join(energy.sparse, by = 'IPC')
+  filter(!IPC %in% unique(energy.sparse$IPC))
+
+isolate.community <- data.frame(energy.isolate[,1]) %>% 
+  distinct(membership)
+colnames(isolate.community) <- 'name'
+isolate.community$Class <- 'Community'
+
+isolate.ipc <- data.frame(energy.isolate[,2]) %>% 
+  distinct(IPC) 
+colnames(isolate.ipc) <- 'name'
+isolate.ipc$Class <- 'IPC'
+
+isolate.vertices <- rbind(isolate.community, isolate.ipc) %>% 
+  mutate(Class = as.factor(Class))
+
+isolate.graph <- graph_from_data_frame(energy.isolate, vertices = isolate.vertices, directed = FALSE)
+
+
+CairoPDF('isolation.pdf', 23.96, 10.888)
+
+ggraph(isolate.graph, layout = 'tree'  # 'sugiyama'  
+       ) +
+  geom_edge_link(color = "lightgrey")+ 
+  geom_node_point(aes(colour = Class)) +
+  geom_node_text(aes(label = name, colour = Class), size = 2, repel = TRUE)+
+  #scale_color_discrete() +
+  theme_graph()
+
+dev.off()
